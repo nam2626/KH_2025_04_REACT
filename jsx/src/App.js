adb984dc08bbbb90a90524a94e0d88d9eff05f96
nam2626 <nam2626@naver.com>
2025-07-14 11:20:30 +0900

    이벤트 처리

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import ComponentEx from './components/ComponentEx';
 import NoJSX from './components/NoJSX';
 
 function App() {
   return (
     <div className="App">
       <NoJSX/>
+      <ComponentEx />
     </div>
   );
 }
---
+++ jsx/src/components/ComponentEx.jsx
@@ -0,0 +1,23 @@
+export default function ComponentEx() {
+    //경고창 띄우는 함수
+    const showAlert = () => {
+        alert('경고창이 출력되었습니다!');
+    };
+
+    return <div>
+        <h2>태그는 반드시 닫아야됨</h2>
+        <label htmlFor="id">아이디 : </label>
+        <input type="text" id="id" placeholder="아이디를 입력하세요" />
+        <hr></hr>
+        {/* 버튼 생성 후 경고창 띄우기*/}
+        <button onClick={showAlert}>경고창 출력 - 1</button>
+        <button onClick={(e) => {
+            console.log(e);
+            // alert('경고창이 출력되었습니다!');
+            //입력한 아이디값 경고창으로 출력
+            alert(`입력한 아이디 : ${document.querySelector('#id').value}`);
+
+        }}>경고창 출력 - 2</button>
+
+    </div>;
+}
