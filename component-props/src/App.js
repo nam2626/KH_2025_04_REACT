c43e5420a241efbe0ed91320d8a31a38b61fcc36
nam2626 <nam2626@naver.com>
2025-07-15 10:18:53 +0900

    props 추가

--- component-props/src/App.js
+++ component-props/src/App.js
@@ -3,9 +3,10 @@ import ButtonOK from './components/ButtonOK';
 
 function App() {
   return (
-    <>
-      <ButtonOK />
-    </>
+    <div className="App">
+      <h2>컴포넌트 Props</h2>
+      <ButtonOK data="데이터" var1="확인" attr="속성값"/>
+    </div>
   );
 }
 
--- component-props/src/components/ButtonOK.jsx
+++ component-props/src/components/ButtonOK.jsx
@@ -1,6 +1,10 @@
-export default function ButtonOK() {
+export default function ButtonOK(props) {
+    console.log(props);
+    let { data, var1, attr } = props;
+    console.log(data, var1, attr);
     const func = () => {
-        alert("OK 버튼 클릭");
+        // alert("OK 버튼 클릭");
+        alert(`${props.data} / ${props.var1} / ${props.attr}`);
     }
   return <button onClick={func}>OK</button>;
 }
