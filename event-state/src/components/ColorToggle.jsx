a291feb0e5b2fd6d975d2126e840cef24cc3455d
nam2626 <nam2626@naver.com>
2025-07-16 10:29:23 +0900

    prettier 설정 추가

--- event-state/src/components/ColorToggle.jsx
+++ event-state/src/components/ColorToggle.jsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export default () => {
-    const [toggle, setToggle] = useState(true);
-    const toggleFunction = () => setToggle(!toggle);
-    return (
-        <div>
-            <p style={{color : toggle ? 'red' : 'blue'}}>이 텍스트의 색상을 변경해 보세요!</p>
-            <button onClick={toggleFunction}>글자 색상 변경</button>
-        </div>
-    );
-}
+  const [toggle, setToggle] = useState(true);
+  const toggleFunction = () => setToggle(!toggle);
+  
+  return (
+    <div>
+      <p style={{ color: toggle ? 'red' : 'blue' }}>
+        이 텍스트의 색상을 변경해 보세요!
+      </p>
+      <button onClick={toggleFunction}>글자 색상 변경</button>
+    </div>
+  );
+};
