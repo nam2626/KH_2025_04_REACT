e4449ea9b4633e6213f6a976ea4d613f5cbbef87
nam2626 <nam2626@naver.com>
2025-07-16 10:14:34 +0900

    ColorToggle 예제

--- event-state/src/App.js
+++ event-state/src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import TimeView from './components/TimeView';
 import CountButton from './components/CountButton';
+import ColorToggle from './components/ColorToggle';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{marginBottom:'100px'}}>
       <TimeView/>
       <hr/>
       <CountButton/>
+      <hr/>
+      <ColorToggle/>
     </div>
   );
 }
