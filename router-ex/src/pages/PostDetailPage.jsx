3b3c330c127bbc2b0b4c1bc0b6068e11bfb39d1c
nam2626 <nam2626@naver.com>
2025-07-28 11:54:40 +0900

    수정

--- router-ex/src/pages/PostDetailPage.jsx
+++ router-ex/src/pages/PostDetailPage.jsx
@@ -3,12 +3,13 @@
 import { useParams } from 'react-router-dom';
 
 export default () => {
-  const param = useParams();
-  console.log(param);
+  //  const param = useParams();
+  //  console.log(param);
+  const { id } = useParams();
   return (
     <div>
       <h2>Post Detail Page</h2>
-      <p>게시글 번호 : {}</p>
+      <p>게시글 번호 : {id}</p>
     </div>
   );
 };
