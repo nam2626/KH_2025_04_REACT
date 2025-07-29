82b4ac4ef33a80dc752d64208d88c3316d843137
nam2626 <nam2626@naver.com>
2025-07-29 11:06:34 +0900

    암호 유효성 검사

--- spring-member-app/src/pages/Register.jsx
+++ spring-member-app/src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 export default () => {
   const txtId = useRef(null);
@@ -6,11 +6,24 @@ export default () => {
   const txtPasswdChk = useRef(null);
   const txtName = useRef(null);
   const txtNickName = useRef(null);
-
+  const [passChkResult, setPassChkResult] = useState('');
   const navigate = useNavigate();
   const passCheck = () => {
     const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#%$^&*])[A-Za-z\d!@#$%\^&\*]{8,20}$/;
     console.log(pattern.test(txtPasswd.current.value));
+    const falseStyle = { fontWegiht: 'bold', color: 'red' };
+    const truetyle = { fontWegiht: 'bold', color: 'blue' };
+    if (pattern.test(txtPasswd.current.value)) {
+      if (txtPasswd.current.value == txtPasswdChk.current.value) {
+        setPassChkResult(<span style={truetyle}>두 암호가 동일하게 입력되었습니다.</span>);
+        return true;
+      } else {
+        setPassChkResult(<span style={falseStyle}>두 암호가 일치하지 않습니다.</span>);
+      }
+    } else {
+      setPassChkResult(<span style={falseStyle}>암호가 형식에 일치 하지 않습니다.</span>);
+    }
+    return false;
   };
   return (
     <div>
@@ -23,9 +36,9 @@ export default () => {
           <input type="text" onChange={passCheck} ref={txtPasswd} placeholder="암호를 입력하세요" />
         </li>
         <li>
-          <input type="password" ref={txtPasswdChk} placeholder="암호를 한번 더 입력하세요" />
+          <input type="password" onChange={passCheck} ref={txtPasswdChk} placeholder="암호를 한번 더 입력하세요" />
         </li>
-        <li></li>
+        <li>{passChkResult}</li>
         <li>
           <input type="text" ref={txtName} placeholder="이름을 입력하세요" />
         </li>
