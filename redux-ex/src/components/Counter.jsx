7752431db9a37b6c965afe56ecb1fa39c4ead682
nam2626 <nam2626@naver.com>
2025-07-30 14:56:45 +0900

    나머지 버튼 이벤트 처리

--- redux-ex/src/components/Counter.jsx
+++ redux-ex/src/components/Counter.jsx
@@ -1,18 +1,32 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { increment } from '../store/counterSlice';
+import { decrement, increment, incrementByAmount } from '../store/counterSlice';
+import { useRef } from 'react';
 
 export default () => {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
+  const num = useRef(null);
   return (
     <div>
       <h2>redux 예제</h2>
-      <input type="text" />
-      <button>증가</button>
+      <input type="text" ref={num} />
+      <button
+        onClick={() => {
+          if (isNaN(num.current.value)) {
+            alert('숫자만 입력하세요');
+            num.current.value = '';
+            num.current.focus();
+            return;
+          }
+          dispatch(incrementByAmount(Number(num.current.value)));
+        }}
+      >
+        증가
+      </button>
       <hr />
       <button onClick={() => dispatch(increment())}>+</button>
       <span>{count}</span>
-      <button>-</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </div>
   );
 };
