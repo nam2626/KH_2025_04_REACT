494bc357bbfc07303e37a28040f8a77b9080bd92
nam2626 <nam2626@naver.com>
2025-07-31 14:57:14 +0900

    todo list v1 - 상위 컴포넌트 작성

---
+++ todo-list-v1/src/components/TodoTemplate.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+const list = [
+  { id: 1, text: '1번째 할일', done: false },
+  { id: 2, text: '2번째 할일', done: false },
+  { id: 2, text: '3번째 할일', done: false },
+  { id: 3, text: '4번째 할일', done: false },
+  { id: 4, text: '5번째 할일', done: false },
+];
+
+export default () => {
+  const [todoList, setTodoList] = useState(list);
+  return (
+    <div>
+      <h2>Todo List</h2>
+    </div>
+  );
+};
