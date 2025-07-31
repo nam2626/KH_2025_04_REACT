ee371e9058742ee74df0efdcf07e3378643828df
nam2626 <nam2626@naver.com>
2025-07-31 15:54:26 +0900

    할일 추가 완료

--- todo-list-v1/src/components/TodoForm.jsx
+++ todo-list-v1/src/components/TodoForm.jsx
@@ -1,11 +1,17 @@
 import { useRef } from 'react';
 
-export default () => {
+export default ({ addTodo }) => {
   const txt = useRef(null);
+  const handleSubmit = () => {
+    addTodo(txt.current.value);
+    txt.current.value = '';
+  };
   return (
     <div className="input-group">
       <input type="text" ref={txt} placeholder="할일을 입력하세요" className="form-control" />
-      <button className="btn btn-dark">Submit</button>
+      <button className="btn btn-dark" onClick={handleSubmit}>
+        Submit
+      </button>
     </div>
   );
 };
--- todo-list-v1/src/components/TodoTemplate.jsx
+++ todo-list-v1/src/components/TodoTemplate.jsx
@@ -1,21 +1,25 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
 const list = [
   { id: 1, text: '1번째 할일', done: false },
   { id: 2, text: '2번째 할일', done: false },
-  { id: 2, text: '3번째 할일', done: false },
-  { id: 3, text: '4번째 할일', done: false },
-  { id: 4, text: '5번째 할일', done: false },
+  { id: 3, text: '3번째 할일', done: false },
+  { id: 4, text: '4번째 할일', done: false },
+  { id: 5, text: '5번째 할일', done: false },
 ];
 
 export default () => {
   const [todoList, setTodoList] = useState(list);
+  const num = useRef(todoList.length);
+  const addTodo = (txt) => {
+    setTodoList([...todoList, { id: ++num.current, text: txt, done: false }]);
+  };
   return (
     <div>
       <h2>Todo List</h2>
-      <TodoForm />
+      <TodoForm addTodo={addTodo} />
       <TodoList todoList={todoList} />
     </div>
   );
