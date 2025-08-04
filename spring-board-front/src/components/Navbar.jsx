57b38eae72f9fe311b19553972411383992a9833
nam2626 <nam2626@naver.com>
2025-08-04 16:43:45 +0900

    프로젝트 생성 및 기본 페이지 생성

---
+++ spring-board-front/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+//src/components/NavBar.jsx
+import { Link } from 'react-router-dom';
+export default () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/login">로그인</Link>
+        </li>
+        <li>
+          <Link to="/signup">회원가입</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
