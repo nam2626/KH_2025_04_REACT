446b89f011087e57bde85c85646095952bb93da2
nam2626 <nam2626@naver.com>
2025-08-05 16:21:12 +0900

    메뉴바 작업 중

--- spring-board-front/src/components/Navbar.jsx
+++ spring-board-front/src/components/Navbar.jsx
@@ -2,14 +2,26 @@
 
 import { Link } from 'react-router-dom';
 import '../css/NavBar.css';
+import { useEffect, useState } from 'react';
+import { getUserData, isAuthenticated } from '../utils/authUtil';
 
 export default () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const data = getUserData();
+    if (!data && isAuthenticated()) {
+      setUser(data);
+    }
+  }, []);
+
   return (
     <nav>
       <ul>
         <li>
           <Link to="/">홈</Link>
         </li>
+        {isAuthenticated() && user ? user.role === 'ROLE_ADMIN' ? <></> : <></> : <></>}
         <li>
           <Link to="/login">로그인</Link>
         </li>
