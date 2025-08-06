7ff252c5411f76d0b940a29ada41aa14f59c4df3
nam2626 <nam2626@naver.com>
2025-08-06 14:15:07 +0900

    페이지 번호 출력 작업 준비 완료

--- spring-board-front/src/components/Pagination.jsx
+++ spring-board-front/src/components/Pagination.jsx
@@ -1,4 +1,13 @@
 export default ({ pagging, onPageChange }) => {
+  //데이터가 없으면 아무것도 렌더링 하지 않음
+  if (!pagging || Object.keys(pagging).length == 0) return null;
+
+  const { currentPage, currentPageGroupNo, endPageOfPageGroup, nextPageGroup, pageOfContentCount, priviousPageGroup, startPageOfPageGroup, totalPage, totalPageGroup } = pagging;
+
+  //표시할 페이지 번호 배열 생성
+  const pageNumbers = [];
+  for (i = startPageOfPageGroup; i <= endPageOfPageGroup; i++) pageNumbers.push(i);
+
   return (
     <>
       {/* 이전 페이지 그룹 ◀ */}
